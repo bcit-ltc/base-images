2a1d4b0a2d85a4c0a43ba33cb5a59941f4b1506f
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-03-26 08:40:33 -0700

    install dagger

---
+++ dagger/src/base_images/main.py
@@ -0,0 +1,31 @@
+import dagger
+from typing import Annotated
+
+from dagger import DefaultPath, dag, function, object_type
+
+
+@object_type
+class BaseImages:
+    @function
+    async def publish(self, source: Annotated[dagger.Directory, DefaultPath("./")]) -> str:
+        """Publish the application container"""
+        
+        # Publish the image for each tag
+        return await self.build(source).publish(
+            f"ghcr.io/bcit-ltc/base-images-semantic-release:latest"
+        )
+
+    @function
+    def build(
+        self,
+        source: Annotated[dagger.Directory, DefaultPath("/")],
+    ) -> dagger.Container:
+        """Build the application container"""
+        build = (
+            dag.container()
+            .from_("node:20-alpine3.21")
+            .with_exec(["apk", "add", "git"])
+            .with_exec(["npm", "install", "-g", "semantic-release", "@semantic-release/github", "@semantic-release/exec"])
+        )
+        return build
+    
